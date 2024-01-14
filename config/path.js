ae7f89d4e2ff289fd116319711279331d60acd39
Valerii <valekutce@gmail.com>
2024-01-14 20:51:05 +0200

    Новые правки

--- config/path.js
+++ config/path.js
@@ -13,37 +13,37 @@ module.exports = {
     css: {
         src: pathSrc + "/assets/css/*.css",
         watch: pathSrc + "/assets/css/*.css",
-        dist: pathDist + "/css"
+        dist: pathDist + "/assets/css"
     },
 
     cass: {
         src: pathSrc + "/assets/Scss/**/*.{scss, sass}",
         watch: pathSrc + "/assets/Scss/**/*.{scss, sass}",
-        dist: pathDist + "/css"
+        dist: pathDist + "/assets/css"
     },
 
     js: {
         src: pathSrc + "/assets/js/*.js",
         watch: pathSrc + "/assets/js/*.js",
-        dist: pathDist + "/js"
+        dist: pathDist + "/assets/js"
     },
 
     img: {
         src: pathSrc + "/assets/img/*.{png,jpg,jpeg,gif,svg}",
         watch: pathSrc + "/assets/**/*.{png,jpg,jpeg,gif,svg}",
-        dist: pathDist + "/img"
+        dist: pathDist + "/assets/img"
     },
 
     font: {
         src: pathSrc + "/assets/fonts/*.{eot,ttf,otf,ttc,otc,woff,woff2,svg}",
         watch: pathSrc + "/assets/**/*.{eot,ttf,otf,ttc,otc,woff,woff2,svg}",
-        dist: pathDist + "/fonts"
+        dist: pathDist + "/assets/fonts"
     },
 
     video: {
         src: pathSrc + "/assets/video/**",
         watch: pathSrc + "/assets/video/**",
-        dist: pathDist + "/video"
+        dist: pathDist + "/assets/video"
     },
     lib: {
         src: pathSrc + "/assets/libs/**",
